be3caf041a972fdbe3658922af5ba9730e874ed0
Arun J <arunj59@hotmail.com>
2019-11-23 16:09:37 +0530

    Update bot.py
    Test commit

--- whatsapp_chatbot/bot.py
+++ whatsapp_chatbot/bot.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!'
 
 
 @app.route('/bot', methods=['POST'])
